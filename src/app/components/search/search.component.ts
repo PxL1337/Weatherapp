0c19363b4200f362ff24716a643093891ac8ea0e
Valentin Pradel <val.pxl.vp@gmail.com>
2023-07-03 11:30:31 +0200

    Fixed input in dark mode and error message on empty input

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -7,14 +7,20 @@ import {WeatherService} from "../../services/weather.service";
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-  city = "";
+  city = '';
+  errorMessage: string = '';
 
   constructor(private weatherService: WeatherService) {
   }
   searchCity() {
-    console.log("Recherche de la ville : ", this.city);
-    this.weatherService.getCurrentMeteo(this.city);
-    this.weatherService.getForecastMeteoByCity(this.city);
+    if (!this.city) {
+      this.errorMessage = 'Veuillez entrer le nom d\'une ville.';
+      return;
+    }else {
+      console.log("Recherche de la ville : ", this.city);
+      this.weatherService.getCurrentMeteo(this.city);
+      this.weatherService.getForecastMeteoByCity(this.city);
+    }
   }
 
   getLocation(): void {
@@ -31,53 +37,8 @@ export class SearchComponent {
       console.error("No support for geolocation")
     }
   }
-  // searchForm: FormGroup | any;
-  // units = ['metric', 'imperial'];
-  //
-  // constructor(private fb: FormBuilder, private weatherService : WeatherService) { }
-  //
-  // ngOnInit(): void {
-  //   this.searchForm = this.fb.group({
-  //     city: [''],
-  //     unit: ['metric']
-  //   });
-  // }
-  //
-  // onSearch(): void {
-  //   const city = this.searchForm.get('city').value;
-  //   const unit = this.searchForm.get('unit').value;
-  //
-  //   this.weatherService.getWeatherByCityName(city)
-  //     .subscribe(data => {
-  //       // Faire quelque chose avec les données
-  //     }, error => {
-  //       alert(error);
-  //     });
-  // }
-  //
-  // onGeolocation(): void {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition((position) => {
-  //       const lat = position.coords.latitude;
-  //       const lon = position.coords.longitude;
-  //       const unit = this.searchForm.get('unit').value;
-  //
-  //       this.weatherService.getCurrentWeather(lat, lon)
-  //         .subscribe(data => {
-  //           // Faire quelque chose avec les données
-  //         }, error => {
-  //           alert(error);
-  //         });
-  //
-  //       this.weatherService.getWeatherForecast(lat, lon)
-  //         .subscribe(data => {
-  //           // Faire quelque chose avec les données
-  //         }, error => {
-  //           alert(error);
-  //         });
-  //     });
-  //   } else {
-  //     alert('Geolocation is not supported by this browser.');
-  //   }
-  // }
+
+  onInput() {
+    this.errorMessage = '';
+  }
 }
